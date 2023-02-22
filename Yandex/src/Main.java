4a94e830cf56ed168b5db2e0bdcf5d8f9e80a9e5
zub <zub@kirka.com>
2023-02-22 18:46:23 +0400

    My second changes

--- Yandex/src/Main.java
+++ Yandex/src/Main.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var scan = new Scanner(System.in);
-        var help = new Helpfull_file();
-        System.out.println("Hello, user!");
-        help.help(scan.nextLine());
+        System.out.println("Hello!");
     }
 }
